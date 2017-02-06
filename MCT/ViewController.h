2c7f7ea76373f9b14a3995ce5c606cd09060fbe9
Yin Yang <yin_yang@apple.com>
2017-02-05 22:04:00 -0800

    Support simple capture of front cam image

--- MCT/ViewController.h
+++ MCT/ViewController.h
@@ -8,7 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIImageView *photoView;
+
+- (IBAction)takePhoto:  (UIButton *)sender;
+- (IBAction)selectPhoto:(UIButton *)sender;
 
 
 @end
--- MCT/ViewController.m
+++ MCT/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  MCT
 //
-//  Created by Yin Yang on 2/5/17.
+//  Created by Judy on 2/5/17.
 //  Copyright © 2017 Judy. All rights reserved.
 //
 
@@ -17,12 +17,73 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        UIAlertController *noCamAlertController = [UIAlertController alertControllerWithTitle:@"No Camera"
+                                                                                      message: @"Camera is Needed for this App"
+                                                                               preferredStyle:UIAlertControllerStyleAlert                   ];
+        [self presentViewController:noCamAlertController animated:YES completion:nil];
+        
+    }
+    
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePhoto:(UIButton *)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = NO;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+    picker.showsCameraControls = YES;   // There is a bug in iOS 10. Need this setting before manual flash control.
+    picker.cameraFlashMode = UIImagePickerControllerCameraFlashModeOn;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+
+- (IBAction)selectPhoto:(UIButton *)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = NO;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    //picker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+
+}
+
+
+/* Fullfill delegate of UIImagePickerController */
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerOriginalImage];
+    self.photoView.image = chosenImage;
+    
+    NSLog(@"Successfully acquired user image.");
+    NSLog(@"Image width %f", chosenImage.size.width);
+    NSLog(@"Image height %f", chosenImage.size.height);
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
 }
 
 
